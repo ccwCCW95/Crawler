b249e72a38df9eb950f875e647dc06491bd79e57
ccwCCW95 <ccw15111840702@gmail.com>
2021-11-25 22:42:08 +1000

    Add the prase step

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import re
 
 def main():
     baseurl = "https://movie.douban.com/top250?start="
     # 1. craw the website
-    # datalist = getData(baseurl=baseurl)
+    datalist = getData(baseurl=baseurl)
+
+findlink = re.compile(r'<a href="(.*?)">')
+findImgSrc = re.compile(r'<img.*src="(.*?)"', re.S)
+findTitle = re.compile(r'<span class="title">(.*)</span>')
+findRating = re.compile(r'<span class="rating_num" property="v:average">(.*)</span>')
+findJudge = re.compile(r'<span>(\d*)人评价</span>')
+findInq = re.compile(r'<span class="inq">(.*)</span>')
+findBd = re.compile(r'<p class="">(.*?)</p>',re.S)
 
 
 def getData(baseurl):
@@ -15,7 +24,51 @@ def getData(baseurl):
         html = askURL(url)
 
         # 2. Parse the data from url
-        
+        soup = BeautifulSoup(html, "html.parser")
+        for item in soup.find_all('div', class_ = "item"):
+            # print(item)
+            data = []
+            item = str(item)
+
+            link = re.findall(findlink, item)[0]
+            data.append(link)
+
+            imgSrc = re.findall(findImgSrc, item)[0]
+            data.append(imgSrc)
+
+            titles = re.findall(findTitle, item)
+            if len(titles) == 2:
+                ctitle = titles[0]
+                data.append(ctitle)
+                otitle = titles[1].replace("/", "")
+                data.append(otitle)
+            else:
+                data.append(titles[0])
+                data.append(' ')
+
+            rating = re.findall(findRating, item)[0]
+            data.append(rating)
+
+            judgeNum = re.findall(findJudge, item)[0]
+            data.append(judgeNum)
+
+            inq = re.findall(findInq, item)[0]
+            if len(inq) != 0:
+                inq = inq[0].replace("。", "")
+                data.append(inq)
+            else:
+                data.append(' ')
+
+            bd = re.findall(findBd, item)[0]
+            bd = re.sub('<br(\s+)?/>(\s+)?', " ", bd)
+            bd = re.sub('/', " ", bd)
+            data.append(bd.strip())
+
+            datalist.append(data)
+
+    return datalist
+
+
 
 # Get the content from a specific url
 def askURL(url):
@@ -29,7 +82,7 @@ def askURL(url):
     try:
         response = urllib.request.urlopen(request)
         html = response.read().decode("utf-8")
-        print(html)
+        # print(html)
     except urllib.error.URLError as e:
         if hasattr(e, "code"):
             print(e.code)
@@ -42,4 +95,4 @@ def askURL(url):
 
 if __name__ == "__main__":
     print("------------------")
-    main()
+    main() 
