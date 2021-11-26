991b60f423f38457e54ae497e728f6536f5914df
ccwCCW95 <ccw15111840702@gmail.com>
2021-11-26 14:47:46 +1000

    Add the record stage(Excel)

--- test.py
+++ test.py
@@ -1,11 +1,24 @@
+'''
+Author: Changwei Cao
+Date: 2021-11-24 18:40:17
+LastEditors: Changwei Cao
+LastEditTime: 2021-11-26 14:46:48
+Description: Test for Python Clawler
+'''
+
 from bs4 import BeautifulSoup
 import urllib.request
 import re
+import xlwt
 
 def main():
     baseurl = "https://movie.douban.com/top250?start="
     # 1. craw the website
     datalist = getData(baseurl=baseurl)
+    savepath = "./MovieTop250.xls"
+
+    # 3. record the data
+    saveData(datalist=datalist, savepath=savepath)
 
 findlink = re.compile(r'<a href="(.*?)">')
 findImgSrc = re.compile(r'<img.*src="(.*?)"', re.S)
@@ -15,7 +28,12 @@ findJudge = re.compile(r'<span>(\d*)人评价</span>')
 findInq = re.compile(r'<span class="inq">(.*)</span>')
 findBd = re.compile(r'<p class="">(.*?)</p>',re.S)
 
-
+'''
+description: Get the data from url and prase them
+param {*} baseurl
+return {*}
+author: Changwei Cao
+'''
 def getData(baseurl):
     datalist = []
 
@@ -52,7 +70,7 @@ def getData(baseurl):
             judgeNum = re.findall(findJudge, item)[0]
             data.append(judgeNum)
 
-            inq = re.findall(findInq, item)[0]
+            inq = re.findall(findInq, item)
             if len(inq) != 0:
                 inq = inq[0].replace("。", "")
                 data.append(inq)
@@ -69,8 +87,12 @@ def getData(baseurl):
     return datalist
 
 
-
-# Get the content from a specific url
+'''
+description: Get the content from a specific url
+param {*} url
+return {*}
+author: Changwei Cao
+'''
 def askURL(url):
     head = {
        "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36" 
@@ -91,8 +113,32 @@ def askURL(url):
 
     return html
 
+'''
+description: Save the data to the Excel
+param {*} datalist
+param {*} savepath
+return {*}
+author: Changwei Cao
+'''
+def saveData(datalist, savepath):
+    book = xlwt.Workbook(encoding="utf-8", style_compression=0)
+    sheet = book.add_sheet('MovieTop250', cell_overwrite_ok=True)
+    col = ("MovieDetails","ImgLink","Chinese Name","English Name","Score","ScoreNum","Abstract","Related Info")
+
+    for i in range(0,8):
+        sheet.write(0,i,col[i])
+
+    for i in range(0,250):
+        print("Num %d"%i)
+        data = datalist[i]
+        for j in range(0,8):
+            sheet.write(i+1,j,data[j])
+
+    book.save(savepath)
+
 
 
 if __name__ == "__main__":
     print("------------------")
-    main() 
+    main()
+    print("Crawler Over")
